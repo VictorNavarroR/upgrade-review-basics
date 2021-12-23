be591fef4cfcb4461b068d20feefee6a2bf992ab
Victor Navarro <victor.navarro@bootcamp-upgrade.com>
2021-12-23 19:00:35 +0100

    Iteration#2

--- index.js
+++ index.js
@@ -13,3 +13,51 @@ for(let movie of movies) {
 categories = [...new Set(categories)];
 
 console.log(categories)
+
+
+//Iteration#2
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+
+function calcMediaOfVoulume(users) {
+  let counter = 0;
+  let sumOfVolumes = 0;
+  for(let user of users) {
+     for(let sound in user.favoritesSounds) {
+       counter += 1;
+       sumOfVolumes += user.favoritesSounds[sound].volume;
+     }
+  }
+  const media = sumOfVolumes / counter;
+  return media;
+}
+
+console.log(calcMediaOfVoulume(users));
