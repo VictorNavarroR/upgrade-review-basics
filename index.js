3b6547bfe1a3749c8211400fa6607a5177a5f2b7
Victor Navarro <victor.navarro@bootcamp-upgrade.com>
2021-12-27 12:09:25 +0100

    Iteration#3

--- index.js
+++ index.js
@@ -61,3 +61,52 @@ function calcMediaOfVoulume(users) {
 }
 
 console.log(calcMediaOfVoulume(users));
+
+//Iteration#3
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+function countFavorites(userArr) {
+  let soundsArr = [];
+  for(let user of users) { 
+      for(let sound in user.favoritesSounds) {     
+          soundsArr.push(sound);
+      }
+  }
+
+  const soundCount = {};
+  soundsArr.forEach((sound) => {
+    soundCount[sound] = (soundCount[sound] ?? 0) + 1;
+  });
+  
+  return soundCount;
+}
+
+console.log(countFavorites(users));
